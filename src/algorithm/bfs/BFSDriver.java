a316d7f14ecb6cfb5b61e76c3025eea0d75ee279
bongki <bongki.cho07@gmail.com>
2017-03-29 18:48:28 +0900

    test

--- src/algorithm/bfs/BFSDriver.java
+++ src/algorithm/bfs/BFSDriver.java
@@ -1,7 +1,102 @@
 package algorithm.bfs;
 
-/**
- * Created by JungHyun on 2017-03-27.
- */
+import graph.DirectedGraph;
+import graph.GraphPartition;
+import graph.NodePartition;
+import task.*;
+import thread.*;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.function.DoubleBinaryOperator;
+
 public class BFSDriver {
+    int level;
+    int numThreads;
+
+    DirectedGraph graph;
+    GraphPartition graphPartition;
+    DoubleBinaryOperator updateFunction;
+    LinkedBlockingQueue<DoubleTask> taskQueue;
+    TaskWaitingRunnable runnable;
+    CyclicBarrier barriers;
+
+    DoubleTask[] workTasks;
+    DoubleTask[] barrierTasks;
+
+    public BFSDriver(DirectedGraph graph, int numThreads) {
+        this.graph = graph;
+        this.numThreads = numThreads;
+        this.level = 0;
+
+        graphPartition = graph.getPartitionInstance();
+        init();
+    }
+
+    public void init() {
+        After3level();
+        GraphPartition graphPartition = graph.getPartitionInstance();
+        int numPartitions = graphPartition.getNumPartitions();
+
+        updateFunction = getUpdateFunction();
+        NodePartition.setUpdateFunction(updateFunction);
+
+        workTasks = new DoubleTask[numPartitions];
+        barrierTasks = new DoubleTask[numThreads];
+        barriers = new CyclicBarrier(numThreads);
+
+        taskQueue = new LinkedBlockingQueue<>();
+        runnable = new TaskWaitingRunnable(taskQueue);
+
+        ThreadUtil.createAndStartThreads(numThreads, runnable);
+
+        for (int i = 0; i < numPartitions; i++) {
+            workTasks[i] = new DoubleTask(i, new BFSExecutor(graph));
+        }
+
+        for (int i = 0; i < numThreads; i++) {
+            barrierTasks[i] = new DoubleTask(i, new TaskBarrier(i, barriers));
+        }
+    }
+
+    public void run() {
+        BFSExecutor.setLevel(1);
+        graphPartition.getPartition(0).setVertexValue(0, 1);
+        for (int i = 0; i < 20; i++) {
+            runOnce(workTasks);
+            runOnce(barrierTasks);
+        }
+        busyWaitForSyncStopMilli(10);
+    }
+
+    public void runOnce(DoubleTask[] tasks) {
+        for (int i = 0; i < tasks.length; i++) {
+            taskQueue.add(tasks[i]);
+        }
+    }
+
+    public void busyWaitForSyncStopMilli(int millisecond) {
+        while (taskQueue.size() != 0) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(millisecond);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public DoubleBinaryOperator getUpdateFunction() {
+        DoubleBinaryOperator updateFunction = (prev, value) -> value;
+        return updateFunction;
+    }
+
+    public void After3level() {
+        //TODO: Add 3 iterations before making parallel process
+    }
+
+    public void reset() {
+        this.level = 0;
+    }
 }
--- src/algorithm/bfs/BFSExecutor.java
+++ src/algorithm/bfs/BFSExecutor.java
@@ -1,7 +1,78 @@
 package algorithm.bfs;
 
-/**
- * Created by JungHyun on 2017-03-27.
- */
-public class BFSExecutor {
+import graph.DirectedGraph;
+import graph.GraphAlgorithmInterface;
+import graph.GraphPartition;
+import graph.NodePartition;
+import graph.Node;
+
+public class BFSExecutor implements GraphAlgorithmInterface {
+
+    DirectedGraph graph;
+    GraphPartition graphPartition;
+    static int currentLevel;
+    int numActiveNodes;
+
+    BFSExecutor(DirectedGraph graph) {
+        this.graph = graph;
+        graphPartition = graph.getPartitionInstance();
+    }
+
+    public static void setLevel(int level) {
+        currentLevel = level;
+    }
+
+    @Override
+    public void execute(int partitionId) {
+        System.out.print("LEVEL : " + currentLevel + "   ");
+        NodePartition partition = graphPartition.getPartition(partitionId);
+        int expOfPartitionSize = graphPartition.getExpOfPartitionSize();
+        int partitionSize = partition.getSize();
+        int offset = partitionId << expOfPartitionSize;
+        numActiveNodes = 0;
+
+
+        for (int i = 0; i < partitionSize; i++) {
+            //xxx
+            int srcNodeId = offset + i;
+            int srcIdPositionInPart = graphPartition.getNodePositionInPart(srcNodeId);
+            double srcCurrentLevel = partition.getVertexValue(srcIdPositionInPart);
+
+            if (srcCurrentLevel == currentLevel) {
+                Node srcNode = graph.getNode(srcNodeId);
+
+                if (srcNode != null) {
+                    update(srcNode);
+                }
+            }
+        }
+        System.out.println(numActiveNodes);
+        currentLevel++;
+
+    }
+
+    public void update(Node srcNode) {
+        int neighborListSize = srcNode.neighborListSize();
+
+        for (int j = 0; j < neighborListSize; j++) {
+            int destId = srcNode.getNeighbor(j);
+            int destPartitionId = graphPartition.getPartitionId(destId);
+
+            NodePartition destPartition = graphPartition.getPartition(destPartitionId);
+
+            int destIdPositionInPart = graphPartition.getNodePositionInPart(destId);
+            double destCurrentLevel = destPartition.getVertexValue(destIdPositionInPart);
+
+            // xxx
+            if (destCurrentLevel == 0 || destCurrentLevel > currentLevel + 1) {
+                destPartition.update(destIdPositionInPart, currentLevel + 1);
+                numActiveNodes++;
+            }
+        }
+    }
+
+    @Override
+    public void reset(int taskId) {
+
+    }
 }
