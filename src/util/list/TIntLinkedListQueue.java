a575287cf85a6d8f8931e4b919c24ced96f205a2
bongki <bongki.cho07@gmail.com>
2017-03-30 13:31:51 +0900

    add TIntLinkedListQueue

---
+++ src/util/list/TIntLinkedListQueue.java
@@ -0,0 +1,52 @@
+package util.list;
+
+import gnu.trove.list.linked.TIntLinkedList;
+
+public class TIntLinkedListQueue extends TIntLinkedList {
+
+    public TIntLinkedListQueue() {
+        super();
+    }
+
+    public int poll() {
+        if (super.size() == 0) {
+            return -1;
+        } else {
+            return super.removeAt(size() - 1);
+        }
+    }
+
+    static class TIntLink {
+        int value;
+        TIntLink previous;
+        TIntLink next;
+
+        TIntLink(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return this.value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public TIntLink getPrevious() {
+            return this.previous;
+        }
+
+        public void setPrevious(TIntLink previous) {
+            this.previous = previous;
+        }
+
+        public TIntLink getNext() {
+            return this.next;
+        }
+
+        public void setNext(TIntLink next) {
+            this.next = next;
+        }
+    }
+}
