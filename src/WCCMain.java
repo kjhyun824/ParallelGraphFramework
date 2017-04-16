3befd7f91a8761e0e219988f00b4fb033f6ee7c7
bongki <bongki.cho07@gmail.com>
2017-04-17 00:32:27 +0900

    WCC Done (v0.3)

--- src/WCCMain.java
+++ src/WCCMain.java
@@ -1,21 +1,29 @@
 import algorithm.wcc.WCCDriver;
 import graph.Graph;
 import graph.GraphUtil;
-import graph.partition.IntegerPartition;
+import graph.partition.WCCPartition;
 
-public class WCCMain {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class WCCMain
+{
     public static void main(String[] args) {
+        final boolean isDirected = false;
+        final boolean isWeighted = false;
         String inputFile = args[0];
         int numThreads = Integer.parseInt(args[1]);
+        double asyncPercent = Double.parseDouble(args[2]);
 
         int expOfPartitionSize = 16; // 1 << 16;
-        int numValuesPerNode = 1;
-        int asyncRangeSize = ((1 << 16) * 0);
+        int asyncRangeSize = (int) ((1 << 16) * asyncPercent);
 
         long start = System.currentTimeMillis();
-        Graph<IntegerPartition> graph = Graph.getInstance(expOfPartitionSize,false,false);
+        Graph<WCCPartition> graph = Graph.getInstance(expOfPartitionSize, isDirected, isWeighted);
         GraphUtil.load(graph, inputFile);
-        graph.generatePartition(numValuesPerNode, asyncRangeSize, IntegerPartition.class);
+        graph.generatePartition(0, asyncRangeSize, WCCPartition.class);
         long loadingTime = System.currentTimeMillis() - start;
 
         System.out.println("[DEBUG] Graph Loading : " + ((double) loadingTime / 1000.0));
@@ -29,10 +37,36 @@ public class WCCMain {
             start = System.currentTimeMillis();
             driver.run();
             elapsedTime[i] = System.currentTimeMillis() - start;
+//            wccValue[i] = driver.getLargestWCC();
             if (i >= 10) {
-                System.out.println("[DEBUG] elapsed time for iteration" + (i-10) + " : " + (((double) elapsedTime[i]) / (1000.0)));
+                System.out.println("[DEBUG] elapsed time for iteration" + (i - 10) + " : " + (((double) elapsedTime[i]) / (1000.0)));
             }
         }
+
+        long timeSum = 0;
+        long max = -1;
+        long min = 1000000;
+
+        for (int i = 0; i < elapsedTime.length - 10; i++) {
+            max = Math.max(max, elapsedTime[i + 10]);
+            min = Math.min(max, elapsedTime[i + 10]);
+            System.out.print(elapsedTime[i + 10] / (double) 1000 + " ");
+            timeSum += elapsedTime[i + 10];
+        }
+        timeSum -= max;
+        timeSum -= min;
+
+        double avg = timeSum / (double) 8;
+        System.out.print("AVG : " + avg);
+
+
+        try (FileWriter fw = new FileWriter(String.valueOf(asyncPercent) + "out.txt", true); BufferedWriter bw = new BufferedWriter(fw); PrintWriter out = new PrintWriter(bw)) {
+            out.print((avg / 1000) + "/");
+        }
+        catch (IOException e) {
+
+        }
+
         System.exit(1);
     }
 }
